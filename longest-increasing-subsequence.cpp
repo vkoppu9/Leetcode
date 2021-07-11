c390ce31203bcd5e8462dd178e95bdb85328800d
vk <kvnr621@gmail.com>
2021-07-11 17:59:30 +0530

    corrected

--- longest-increasing-subsequence.cpp
+++ longest-increasing-subsequence.cpp
@@ -29,7 +29,7 @@ public:
         vector<int>LIS(n, 1);
         
         for (int i = 0; i < n; i++) {
-            for (int j = 1; j < i; j++) {
+            for (int j = 0; j < i; j++) {
                 if (nums[i] > nums[j])
                     LIS[i] = max(LIS[i], 1 + LIS[j]);
             }
